d1e42036509fcfb1a79aee167b0142703efd2646
shivam12k <mynameisshivam78@gmail.com>
2020-10-05 22:43:45 +0530

    adding play/pause funtionality in music player

---
+++ script.js
@@ -0,0 +1,29 @@
+const music = document.querySelector('audio');
+const play = document.querySelector('#play');
+const img = document.querySelector('.album');
+let isplaying = false;
+// play functionality
+const PlayMusic = () => {
+    isplaying = true;
+    music.play();
+    play.classList.replace('fa-play', 'fa-pause')
+    img.classList.replace('album', 'loading');
+
+}
+//pause functionality
+const PauseMusic = () => {
+    isplaying = false;
+    music.pause();
+    play.classList.replace('fa-pause', 'fa-play')
+
+}
+    //toggle
+    play.addEventListener('click', () => {
+        
+        if (isplaying) {
+            PauseMusic();
+        } else {
+            PlayMusic();
+        }
+
+    })
