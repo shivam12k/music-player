58d09f746e6462bac393c8e3a0d81642714fb84f
shivam <63141401+shivam12k@users.noreply.github.com>
2020-10-09 22:28:55 +0530

    adding songs and albums

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const music = document.querySelector('audio');
 const play = document.querySelector('#play');
-const img = document.querySelector('.album');
+const img = document.querySelector('img');
 let isplaying = false;
+let music_name = document.getElementsByTagName('h2');
+let next = document.querySelector('#next');
+let prev = document.querySelector('#prev');
+//array of songs
+const songs = [
+    {
+        name: "Airtel"
+    },
+    {
+        name: "I Like How It Feels"
+    },
+    {
+        name: "Tumko To Aana Hi Tha"
+    },
+
+]
+
 // play functionality
 const PlayMusic = () => {
     isplaying = true;
@@ -10,21 +27,33 @@ const PlayMusic = () => {
     img.classList.replace('album', 'loading');
 
 }
+
 //pause functionality
 const PauseMusic = () => {
     isplaying = false;
     music.pause();
     play.classList.replace('fa-pause', 'fa-play')
-    img.classList.replace( 'loading','album');
+    img.classList.replace('loading', 'album');
 
 }
-    //toggle
-    play.addEventListener('click', () => {
-        
-        if (isplaying) {
-            PauseMusic();
-        } else {
-            PlayMusic();
-        }
 
-    })
+//toggle
+play.addEventListener('click', () => {
+
+    if (isplaying) {
+        PauseMusic();
+    } else {
+        PlayMusic();
+    }
+
+})
+
+//changing the music
+const loadSong = (songs) => {
+    console.log("hello");
+    music_name.textContent = songs.name;
+    music.src = `songs/${songs.name}.mp3`
+    img.src = `albums/${songs.name}.jpg`;
+}
+
+loadSong(songs[2]);
