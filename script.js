e65f7352ecd067defec845edc9a0b5db855b5758
shivam <63141401+shivam12k@users.noreply.github.com>
2020-10-09 22:58:50 +0530

    fix the album bug and add toggling of songs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const music = document.querySelector('audio');
 const play = document.querySelector('#play');
 const img = document.querySelector('img');
 let isplaying = false;
-let music_name = document.getElementsByTagName('h2');
+let music_name = document.querySelector('#name');
 let next = document.querySelector('#next');
 let prev = document.querySelector('#prev');
 //array of songs
@@ -49,11 +49,34 @@ play.addEventListener('click', () => {
 })
 
 //changing the music
+let songIndex = 0;
 const loadSong = (songs) => {
-    console.log("hello");
     music_name.textContent = songs.name;
     music.src = `songs/${songs.name}.mp3`
     img.src = `albums/${songs.name}.jpg`;
 }
+loadSong(songs[songIndex]);
 
-loadSong(songs[2]);
+const nextSong = () => {
+    if (songIndex < 2) {
+        songIndex++;
+        loadSong(songs[songIndex]);
+        PlayMusic();
+    } else {
+        alert("Only three songs are available")
+    }
+
+}
+const prevSong = () => {
+    if (songIndex >= 1) {
+        songIndex--;
+        loadSong(songs[songIndex]);
+        PlayMusic();
+    }
+}
+next.addEventListener('click', nextSong);
+prev.addEventListener('click', prevSong);
+
+
+
+
